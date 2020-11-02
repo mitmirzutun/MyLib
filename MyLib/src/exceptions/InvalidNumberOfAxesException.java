5c4d11c9928a681d663c1026ae4dab56588600fd
milan <milan@Milans-PC>
2020-11-02 07:15:27 +0100

    Added Matrix and Vector

---
+++ MyLib/src/exceptions/InvalidNumberOfAxesException.java
@@ -0,0 +1,5 @@
+package exceptions;
+
+public class InvalidNumberOfAxesException extends IllegalArgumentException {
+
+}
---
+++ MyLib/src/exceptions/InvalidVectorException.java
@@ -0,0 +1,5 @@
+package exceptions;
+
+public class InvalidVectorException extends IllegalArgumentException {
+
+}
